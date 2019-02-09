0498d63d115797e17dfdce6ba70568664fc0821a
Edric Chan <edric.chan.1997@gmail.com>
2019-02-09 22:13:45 +0800

    chore(deprecated-toolbar): remove boilerplate code

--- src/app/partials/deprecated-toolbar/deprecated-toolbar.component.ts
+++ src/app/partials/deprecated-toolbar/deprecated-toolbar.component.ts
@@ -1,14 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-deprecated-toolbar',
   templateUrl: './deprecated-toolbar.component.html'
 })
-export class DeprecatedToolbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class DeprecatedToolbarComponent {}
