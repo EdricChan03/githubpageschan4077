05100a43f84e46b3b6c1a447672533505a95d690
Edric Chan <edric.chan.1997@gmail.com>
2019-02-09 22:12:03 +0800

    chore: add deprecated toolbar

---
+++ src/app/partials/deprecated-toolbar/deprecated-toolbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deprecated-toolbar',
+  templateUrl: './deprecated-toolbar.component.html'
+})
+export class DeprecatedToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
